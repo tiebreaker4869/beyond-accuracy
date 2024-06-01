6d1fb817ff2ffff8f1a9dc182d99de09f90522bc
Lifan Sun <lifansun1412@gmail.com>
2024-06-01 16:03:51 +0800

    add docs

--- tests/metrics/test_accuracy_metrics.py
+++ tests/metrics/test_accuracy_metrics.py
@@ -1,3 +1,4 @@
+
 import pytest
 import math
 from beyond_accuracy.metrics.accuracy_metrics import Precision, Recall, MRR, NDCG
@@ -5,6 +6,12 @@ from beyond_accuracy.metrics.accuracy_metrics import Precision, Recall, MRR, NDC
 
 @pytest.fixture
 def sample_data():
+    """
+    Fixture that provides sample data for the tests.
+
+    Returns:
+        tuple: A tuple containing the recommended list, relevant list, k value, and all items.
+    """
     recommended_list = [1, 2, 3, 4, 5]
     relevant_list = [1, 3, 6, 7]
     k = 3
@@ -13,6 +20,12 @@ def sample_data():
 
 
 def test_precision(sample_data):
+    """
+    Test case for the Precision metric.
+
+    Args:
+        sample_data (tuple): A tuple containing the recommended list, relevant list, k value, and all items.
+    """
     recommended_list, relevant_list, k, all_items = sample_data
     precision = Precision(all_items)
     assert math.isclose(
@@ -21,18 +34,36 @@ def test_precision(sample_data):
 
 
 def test_recall(sample_data):
+    """
+    Test case for the Recall metric.
+
+    Args:
+        sample_data (tuple): A tuple containing the recommended list, relevant list, k value, and all items.
+    """
     recommended_list, relevant_list, k, all_items = sample_data
     recall = Recall(all_items)
     assert math.isclose(recall.compute(recommended_list, relevant_list, [], k), 2 / 4)
 
 
 def test_mrr(sample_data):
+    """
+    Test case for the MRR (Mean Reciprocal Rank) metric.
+
+    Args:
+        sample_data (tuple): A tuple containing the recommended list, relevant list, k value, and all items.
+    """
     recommended_list, relevant_list, k, all_items = sample_data
     mrr = MRR(all_items)
     assert math.isclose(mrr.compute(recommended_list, relevant_list, [], k), 1 / 1)
 
 
 def test_ndcg(sample_data):
+    """
+    Test case for the NDCG (Normalized Discounted Cumulative Gain) metric.
+
+    Args:
+        sample_data (tuple): A tuple containing the recommended list, relevant list, k value, and all items.
+    """
     recommended_list, relevant_list, k, all_items = sample_data
     ndcg = NDCG(all_items)
     golden_answer = (1 / 1 + 1 / 2) / (1 + 1 / math.log2(3) + 1 / 2)
